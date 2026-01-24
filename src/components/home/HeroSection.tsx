5cb77d46cae14d29c4d2c692c09608cfede2de43
noursalama109-oss <noursalama109@gmail.com>
2026-01-24 18:01:04 +0200

    Update HeroSection.tsx

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -104,13 +104,12 @@ const HeroSection = () => {
               </div>
               <span className="text-sm font-medium">موتوسيكل</span>
             </div>
-            <svg 
-  xmlns="http://www.w3.org/2000/svg" 
-  viewBox="0 0 640 512" 
-  className="w-8 h-8 text-primary fill-current"
->
-  <path d="M624 352h-16V243.9c0-12.7-5.1-24.9-14.1-33.9L494 110.1c-9-9-21.2-14.1-33.9-14.1H416V48c0-26.5-21.5-48-48-48H112C85.5 0 64 21.5 64 48v48H8c-4.4 0-8 3.6-8 8v16c0 4.4 3.6 8 8 8h272c4.4 0 8 3.6 8 8v16c0 4.4-3.6 8-8 8H40c-4.4 0-8 3.6-8 8v16c0 4.4 3.6 8 8 8h208c4.4 0 8 3.6 8 8v16c0 4.4-3.6 8-8 8H8c-4.4 0-8 3.6-8 8v16c0 4.4 3.6 8 8 8h208c4.4 0 8 3.6 8 8v16c0 4.4-3.6 8-8 8H64v128c0 53 43 96 96 96s96-43 96-96h128c0 53 43 96 96 96s96-43 96-96h16c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zM160 464c-26.5 0-48-21.5-48-48s21.5-48 48-48 48 21.5 48 48-21.5 48-48 48zm320 0c-26.5 0-48-21.5-48-48s21.5-48 48-48 48 21.5 48 48-21.5 48-48 48zm80-208H416V144h44.1l99.9 100v12z"/>
-</svg>
+            <div className="w-px h-12 bg-border" />
+            <div className="flex flex-col items-center gap-2 text-muted-foreground">
+              <div className="w-14 h-14 rounded-xl bg-secondary flex items-center justify-center">
+                <Truck className="w-7 h-7 text-primary" />
+              </div>
+<span className="text-sm font-medium">تروسيكل</span>
             </div>
           </div>
         </div>
