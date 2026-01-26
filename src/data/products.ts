0c994d6e7f3d90e227d710bf6465a8b59302e782
noursalama109-oss <noursalama109@gmail.com>
2026-01-26 16:16:27 +0200

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -64,7 +64,7 @@ export const products: Product[] = [
     vehicleType: 'motorcycle',
     brand: 'other',
     section: 'electrical',
-    subSection: 'ignition',
+    subSection: 'starter',
     description: 'موبينة مارش قوية ومتينة',
     inStock: true,
   },
